7a60c7d72707d708abd2618b153fdc31f8da06f8
James Burgess <goldfibre@gmail.com>
2015-11-15 22:18:04 +0100

    static render

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+
+//app = express();
+app.use(express.logger('dev'));
+app.use('/', express.static('./public'));
+  /*
+app.get('/search', function (req, res) {
+  data.search(req.params, req.query, function(results) { res.json(results) });
+});
+            /*
+app.listen(8080);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,7 +23,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-        */
+
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
@@ -23,3 +33,4 @@ app.listen(app.get('port'), function() {
 });
 
 
+              */
