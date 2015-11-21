d09a42a511604cccb44a2709795aba9c83861326
James Burgess <goldfibre@gmail.com>
2015-11-21 17:10:03 +0100

    added assert

--- index.js
+++ index.js
@@ -13,31 +13,6 @@ if (env === 'development') {
     url = 'mongodb://jburgess:Cdjuices1@ds057254.mongolab.com:57254/heroku_3zthx5rr';
 }
 
-var getDateTime = function() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year + "/" + month + "/" + day + " " + hour + ":" + min + ":" + sec;
-
-};
-
 var insertDocument = function(db, params, callback) {
     var record = {
         date: new Date()
