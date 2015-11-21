f6a3693da212efb6677a93bfec175bf521100692
James Burgess <goldfibre@gmail.com>
2015-11-21 09:02:06 +0100

    static render

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-//var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
-//app = express();
-app.use(express.logger('dev'));
-app.use('/', express.static('./public'));
-  /*
-app.get('/search', function (req, res) {
-  data.search(req.params, req.query, function(results) { res.json(results) });
-});
-            /*
-app.listen(8080);
-
 app.set('port', (process.env.PORT || 5000));
-
+        /*
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-  /*
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
@@ -28,9 +17,15 @@ app.get('/cool', function(request, response) {
   response.send(cool());
 });
 
+app.get('/cooler', function(request, response) {
+  response.send('Hello world!');
+});
+*/
+
+app.use('/', express.static('./views/pages'));
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 
-              */
